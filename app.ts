43964d2ad78d9e4e596b14027d9cb2ff12fee53a
NepaliMentor <sumantiwari049@gmail.com>
2024-01-30 22:04:58 +0545

    api

--- app.ts
+++ app.ts
@@ -19,16 +19,9 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://nepalimentor.com');
-  // Add other CORS headers as needed (e.g., methods, headers, etc.)
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
+app.use(cors({ origin: true, credentials: true }));
 
-  // Continue to the next middleware or route handler
-  next();
-});
+
 // api requests limit
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
