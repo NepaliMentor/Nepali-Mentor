78106245d2e8c6667af08f4f27495eb97a2a7887
NepaliMentor <sumantiwari049@gmail.com>
2024-01-30 21:02:07 +0545

    fix

--- app.ts
+++ app.ts
@@ -21,32 +21,10 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: "https://nepalimentor.com",
+    origin: ["https://nepalimentor.com/", "http://localhost:3000"],
     credentials: true,
   })
 );
-// Set middleware of CORS 
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://nepalimentor.com/"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Private-Network", "true");
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", "7200");
-
-  next();
-});
-
 // api requests limit
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
--- build/app.js
+++ build/app.js
@@ -23,20 +23,9 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross origin resource sharing
 exports.app.use((0, cors_1.default)({
-    origin: "https://nepalimentor.com",
+    origin: ["https://nepalimentor.com/", "http://localhost:3000"],
     credentials: true,
 }));
-// Set middleware of CORS 
-exports.app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://nepalimentor.com/");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Private-Network", "true");
-    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-    res.setHeader("Access-Control-Max-Age", "7200");
-    next();
-});
 // api requests limit
 const limiter = (0, express_rate_limit_1.rateLimit)({
     windowMs: 15 * 60 * 1000,
