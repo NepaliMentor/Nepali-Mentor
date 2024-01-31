4e76efad8e444fc3bc7f684b0b54b59d7ba5a130
NepaliMentor <sumantiwari049@gmail.com>
2024-01-31 09:37:01 +0545

    lc

--- app.ts
+++ app.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // cors => cross origin resource sharing
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://nepalimentor.com", "https://www.nepalimentor.com","https://nepalimentor.com/", "http://www.nepalimentor.com","https://nepali-mentor-production.up.railway.app/","https://nepali-mentor-production.up.railway.app", "https://www.nepali-mentor-production.up.railway.app/"],
+    origin: ["http://localhost:3000","http://localhost:3000/","https://nepalimentor.com", "https://www.nepalimentor.com","https://nepalimentor.com/", "http://www.nepalimentor.com","https://nepali-mentor-production.up.railway.app/","https://nepali-mentor-production.up.railway.app", "https://www.nepali-mentor-production.up.railway.app/"],
     methods: '*', // Allow all HTTP methods
     optionsSuccessStatus: 204,
     credentials: true,
