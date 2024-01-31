525c2f6fe0296767dee585feff0fdc0b5048edce
NepaliMentor <sumantiwari049@gmail.com>
2024-01-31 17:58:21 +0545

    Mail

--- build/app.js
+++ build/app.js
@@ -7,6 +7,7 @@ exports.app = void 0;
 require("dotenv").config();
 const express_1 = __importDefault(require("express"));
 exports.app = (0, express_1.default)();
+const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const error_1 = require("./middleware/error");
 const user_route_1 = __importDefault(require("./routes/user.route"));
@@ -20,15 +21,7 @@ const express_rate_limit_1 = require("express-rate-limit");
 exports.app.use(express_1.default.json({ limit: "50mb" }));
 // cookie parser
 exports.app.use((0, cookie_parser_1.default)());
-exports.app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://nepalimentor.com');
-    // Add other CORS headers as needed (e.g., methods, headers, etc.)
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    // Continue to the next middleware or route handler
-    next();
-});
+exports.app.use((0, cors_1.default)({}));
 // api requests limit
 const limiter = (0, express_rate_limit_1.rateLimit)({
     windowMs: 15 * 60 * 1000,
--- build/middleware/error.js
+++ build/middleware/error.js
@@ -1,34 +1,43 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ErrorMiddleware = void 0;
-const ErrorHandler_1 = __importDefault(require("../utils/ErrorHandler"));
 const ErrorMiddleware = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal server error";
     // wrong mongodb id error
     if (err.name === "CastError") {
         const message = `Resource not found. Invalid: ${err.path}`;
-        err = new ErrorHandler_1.default(message, 400);
+        return res.status(400).json({
+            success: false,
+            message: message,
+        });
     }
     // Duplicate key error
     if (err.code === 11000) {
         const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
-        err = new ErrorHandler_1.default(message, 400);
+        return res.status(400).json({
+            success: false,
+            message: message,
+        });
     }
     // wrong jwt error
     if (err.name === "JsonWebTokenError") {
         const message = `Json web token is invalid, try again`;
-        err = new ErrorHandler_1.default(message, 400);
+        return res.status(400).json({
+            success: false,
+            message: message,
+        });
     }
     // JWT expired error
     if (err.name === "TokenExpiredError") {
         const message = `Json web token is expired, try again`;
-        err = new ErrorHandler_1.default(message, 400);
+        return res.status(400).json({
+            success: false,
+            message: message,
+        });
     }
-    res.status(err.statusCode).json({
+    // Handle other errors
+    return res.status(err.statusCode).json({
         success: false,
         message: err.message,
     });
