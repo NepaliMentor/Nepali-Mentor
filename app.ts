c52b609e56a6aa3e74ebdfb9e5de8b456b685fe6
Suman Tiwari <88159698+NepaliMentor@users.noreply.github.com>
2024-02-12 17:50:16 +0545

    Update app.ts

--- app.ts
+++ app.ts
@@ -18,13 +18,7 @@ app.use(express.json({ limit: "50mb" }));
 // cookie parser
 app.use(cookieParser());
 
-// cors => cross origin resource sharing
-app.use(
-  cors({
-    origin: ["https://nepalimentor.com", "https://nepalimentor.com/api/v1/","https://nepali-mentor-production.up.railway.app", "http://localhost:3000"],
-    credentials: true,
-  })
-);
+app.use(cors()); 
 
 
 
