4854ad651a3bbb3a4a74208656511d29b7a11c33
Suman Tiwari <88159698+NepaliMentor@users.noreply.github.com>
2024-02-22 22:37:27 +0545

    Update app.ts LMS

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://nepalimentor.com","https://nepalimentor.com/"],
+    origin: ["http://localhost:3000","https://nepalimentor.com","https://nepalimentor.com/","https://lms.nepalimentor.com/", "https://lms.nepalimentor.com"],
     credentials: true,
   })
 );
